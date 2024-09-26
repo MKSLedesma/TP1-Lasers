de87cd10c8fcd8f596ec7cace986d6018123d219
MLAGANGA <163701230+MLAGANGA@users.noreply.github.com>
2024-09-26 01:53:19 -0300

    Fix

--- src/main/java/model/Objetivo.java
+++ src/main/java/model/Objetivo.java
@@ -4,13 +4,12 @@ public class Objetivo implements Interfaz {
 
     @Override
     public void interactuarLaser(Laser laser) {
-        // El láser ha alcanzado el objetivo
-        laser.detener(); // Detiene el láser al alcanzar el objetivo
+        laser.detener(); 
         System.out.println("¡Llegaste al objetivo!");
     }
 
     @Override
     public String representacion() {
-        return "O"; // Representación del objetivo en el tablero
+        return "O"; 
     }
 }
